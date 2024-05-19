5ba4cafd46c1a1b3a3e58277cb0332be5deb8b42
mah05rad <154422590+mah05rad@users.noreply.github.com>
2024-05-19 22:40:15 +0330

    Update manager.py

--- manager.py
+++ manager.py
@@ -12,19 +12,23 @@ def create_admin(username, password):
 
             if username == username1:
                 print("Error: The entered information is duplicate!")
+                return
     with open("admin.json", "a") as file:
         file.write(f"{username},{password}\n")
         print("The new admin was successfully registered :)")
 
 def main():
     parser = argparse.ArgumentParser(description="Create system admin")
-    parser.add_argument("create-admin", help="Create system admin", action="store_true")
-    parser.add_argument("--username", help="Username of the admin", required=True)
-    parser.add_argument("--password", help="Password of the admin", required=True)
+    subparsers = parser.add_subparsers(dest='cd', help='cd help')
+    parser_print = subparsers.add_parser("create-admin", help="Create system admin")
+    parser_print.add_argument("--username", help="Username of the admin", required=True)
+    parser_print.add_argument("--password", help="Password of the admin", required=True)
     args = parser.parse_args()
 
-    if args.create_admin:
+    if args.cd =="create-admin" :
         create_admin(args.username, args.password)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
