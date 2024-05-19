861fc1cfc25892a56af0b6d0f29af8566d203244
mah05rad <154422590+mah05rad@users.noreply.github.com>
2024-05-19 22:57:38 +0330

    Update manager.py

--- manager.py
+++ manager.py
@@ -19,13 +19,13 @@ def create_admin(username, password):
 
 def main():
     parser = argparse.ArgumentParser(description="Create system admin")
-    subparsers = parser.add_subparsers(dest='cd', help='cd help')
+    subparsers = parser.add_subparsers(dest="command", help="command help")
     parser_print = subparsers.add_parser("create-admin", help="Create system admin")
     parser_print.add_argument("--username", help="Username of the admin", required=True)
     parser_print.add_argument("--password", help="Password of the admin", required=True)
     args = parser.parse_args()
 
-    if args.cd =="create-admin" :
+    if args.command =="create-admin" :
         create_admin(args.username, args.password)
     else:
         parser.print_help()
