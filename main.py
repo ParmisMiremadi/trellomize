0d178e28eeab0f2c25a281a7d54bed75592b620d
mah05rad <154422590+mah05rad@users.noreply.github.com>
2024-05-21 20:27:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
-import getpass
 import os
+import re
+import getpass
 import time
+import bcrypt
  
 if not os.path.exists("user.json"):
     open("user.json", "w").close()
@@ -10,23 +12,32 @@ def clear_consoule(time1):
     time.sleep(time1)
     os.system("cls" if os.name == "nt" else "clear")
 
+def is_valid_email(email):
+    valid_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    return re.fullmatch(valid_email, email)
+
 def signup():
     email = input("Enter your email: ")
     username = input("Enter your username: ")
     password = getpass.getpass("Enter your password: ")
+    encrypted_password = bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt())
     be_active = True
 
-    with open("user.json", "r") as file:
-        for line in file:
-            email1 = line.strip().split(",")[0]
-            username1 = line.strip().split(",")[1]
+    if not is_valid_email(email):
+        print("Error: Invalid email format!")
+        return
+    else:
+        with open("user.json", "r") as file:
+            for line in file:
+                email1 = line.strip().split(",")[0]
+                username1 = line.strip().split(",")[1]
 
-            if email == email1 or username == username1:
-                print("Error: The entered information is duplicate!")
-                return   
-    with open("user.json", "a") as file:
-        file.write(f"{email},{username},{password},{be_active}\n")
-        print("Sign in successful :)")
+                if email == email1 or username == username1:
+                    print("Error: The entered information is duplicate!")
+                    return   
+        with open("user.json", "a") as file:
+            file.write(f"{email},{username},{encrypted_password.decode("utf-8")},{be_active}\n")
+            print("Sign in successful :)")
 
 def login():
     username = input("Enter your username: ")
@@ -40,7 +51,7 @@ def login():
             be_active1 = line.strip().split(",")[3]
             
             if username == username1:
-                if password == password1:
+                if bcrypt.checkpw(password.encode("utf-8"), password1.encode("utf-8")):
                     if be_active != bool(be_active1):
                         print("Error: You don't have access to your account!")
                         return
