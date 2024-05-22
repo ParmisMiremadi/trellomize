2821de617d0e729e39bb4a397a563d574046cc40
mah05rad <154422590+mah05rad@users.noreply.github.com>
2024-05-22 20:06:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ class User:
         self.email = email
         self.username = username
         self.__password = password
+        self.be_active = True
         self.projects_as_leader = []
         self.projects_as_member = []
-        self.be_active = True
 
 
 def clear_console(time1):
@@ -30,26 +30,30 @@ def is_valid_email(email):
 
 
 def sign_up():
-    email = input("Enter your email: ")
+    while True:
+        email = input("Enter your email: ")
+        if not is_valid_email(email):
+            print("Error: Invalid email format!")
+            clear_console(2)
+        else:
+            break
+        
     username = input("Enter your username: ")
     password = getpass.getpass("Enter your password: ")
     encrypted_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     user_obj = User(email, username, encrypted_password)
-    if not is_valid_email(email):
-        print("Error: Invalid email format!")
-        return
-    else:
-        with open("user.json", "r") as file:
-            for line in file:
-                email1 = line.strip().split(",")[0]
-                username1 = line.strip().split(",")[1]
+    
+    with open("user.json", "r") as file:
+        for line in file:
+            email1 = line.strip().split(" ; ")[0]
+            username1 = line.strip().split(" ; ")[1]
 
-                if email == email1 or username == username1:
-                    print("Error: The entered information is duplicate!")
-                    return
-        with open("user.json", "a") as file:
-            file.write(f"{email},{username},{encrypted_password.decode("utf-8")},{user_obj.be_active}\n")
-            print("Your sign in was successful :)")
+            if email == email1 or username == username1:
+                print("Error: The entered information is duplicate!")
+                return
+    with open("user.json", "a") as file:
+        file.write(f"{email} ; {username} ; {encrypted_password.decode("utf-8")} ; {user_obj.be_active} ; {user_obj.projects_as_leader} ; {user_obj.projects_as_member}\n")
+        print("Your sign in was successful :)")
     return user_obj
 
 
@@ -59,9 +63,9 @@ def log_in():
 
     with open("user.json", "r") as file:
         for line in file:
-            username1 = line.strip().split(",")[1]
-            password1 = line.strip().split(",")[2]
-            be_active1 = line.strip().split(",")[3]
+            username1 = line.strip().split(" ; ")[1]
+            password1 = line.strip().split(" ; ")[2]
+            be_active1 = line.strip().split(" ; ")[3]
             true_bool = True
             if username == username1:
                 if bcrypt.checkpw(password.encode("utf-8"), password1.encode("utf-8")):
@@ -70,10 +74,10 @@ def log_in():
                         return
                     else:
                         print("Your log in was successful :)")
-                        user_obj = User(line.strip().split(",")[0], username1, password1)
+                        user_obj = User(line.strip().split(" ; ")[0], username1, password1)
                         return user_obj
                 else:
                     print("Error: The password is invalid!")
                     return
         print("Error: Username not found!")
-        
+
