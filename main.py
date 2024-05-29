5bb41f9ce9ca475cb02138aacdf29458eaf35146
ParmisMiremadi <154410754+ParmisMiremadi@users.noreply.github.com>
2024-05-29 23:14:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
-import random
 import os
 import time
-import user
 from user import User as User
 from user import clear_console as clear_console
 from user import log_in
@@ -20,16 +18,16 @@ def pr_green(skk): print("\033[32m {}\033[00m".format(skk))
 def pr_red(skk): print("\033[31m {}\033[00m".format(skk))
 
 
-if not os.path.exists('projects.json'):
-    with open('projects.json', 'w') as file:
+if not os.path.exists("projects.json"):
+    with open("projects.json", "w") as file:
         json.dump([], file, indent=4)
     file.close()
 
 user = User("", "", "")
-projects_file_path = 'projects.json'
+projects_file_path = "projects.json"
 projects_list = projects.projects_list
 my_project = Project("", "", user)
-users_list = projects.load_projects_from_file('user.json')
+users_list = projects.load_projects_from_file("user.json")
 
 if len(projects.admin_list) > 0:
     admin_projects_as_leader = projects.admin_projects_as_leader  # List of dictionaries
@@ -62,16 +60,16 @@ while run == true_bool:
         clear_console(2)
 
     elif isinstance(user, int) and user == 0:
-        print('Exiting program...')
+        print("Exiting program...")
         clear_console(2)
-        pr_red('Exit code: 1')
+        pr_red("Exit code: 1")
         run = not true_bool
 
     elif user.is_admin != true_bool:
         choice_1 = "0"  # 1. New project  2. My projects  3. Exit
         while choice_1 != "3":
-            print('Choose one of the following options.')
-            pr_cyan('1. New project\n 2. My projects\n 3. Exit\n')
+            print("Choose one of the following options.")
+            pr_cyan("1. New project\n 2. My projects\n 3. Exit\n")
             choice_1 = (input())
             clear_console(1.5)
 
@@ -80,21 +78,21 @@ while run == true_bool:
                 if isinstance(project_object, Project):
                     user = project_object.to_dict_and_save_to_file(projects_file_path, user)
                     time.sleep(1)
-                    pr_green('Creating a new project')
+                    pr_green("Creating a new project")
                     clear_console(1)
-                    pr_green('Creating a new project.')
+                    pr_green("Creating a new project.")
                     clear_console(1)
-                    pr_green('Creating a new project..')
+                    pr_green("Creating a new project..")
                     clear_console(1)
-                    pr_green('Creating a new project...')
+                    pr_green("Creating a new project...")
                     clear_console(1)
                     # List of dictionaries:
                     projects_list = projects.load_projects_from_file(projects_file_path)
                     print("\033[36m {}\033[32m {}\033[36m {}\033[00m".format
                           ("Project created successfully.\nYou are now the "
-                           "leader of the project", f'{project_object.get_project_title()}', '.'))
+                           "leader of the project", f"{project_object.get_project_title()}", "."))
                     print("\033[36m {}\033[32m {}\033[00m".format
-                          ('Project ID: ', f'{project_object.get_project_id()}'))
+                          ("Project ID: ", f"{project_object.get_project_id()}"))
 
                 else:
                     pass
@@ -115,19 +113,19 @@ while run == true_bool:
 
             elif choice_1 == "3":    # 3. Exit
                 clear_console(1)
-                pr_green('Exiting program...')
+                pr_green("Exiting program...")
                 clear_console(2)
-                pr_green('Exit code: 0')
+                pr_green("Exit code: 0")
                 run = not true_bool
 
             else:
-                print('Invalid choice.\nPlease try again.')
+                print("Invalid choice.\nPlease try again.")
 
     elif user.is_admin == true_bool:
         ch_1 = "-1"  # 1. New project  2. My projects  3. Activate users 4. Deactivate users 5. Exit
         while ch_1 != "0":
-            print('Choose one of the following options.')
-            pr_cyan('1. New project\n 2. My projects\n 3. Activate users\n 4. Deactivate users\n 5. Exit\n')
+            print("Choose one of the following options.")
+            pr_cyan("1. New project\n 2. My projects\n 3. Activate users\n 4. Deactivate users\n 5. Exit\n")
             ch_1 = input()
 
             if ch_1 == "1":  # 1. New project
@@ -135,21 +133,21 @@ while run == true_bool:
                 if isinstance(project_object, Project):
                     user = project_object.to_dict_and_save_to_file(projects_file_path, user)
                     time.sleep(1)
-                    pr_green('Creating a new project')
+                    pr_green("Creating a new project")
                     clear_console(1)
-                    pr_green('Creating a new project.')
+                    pr_green("Creating a new project.")
                     clear_console(1)
-                    pr_green('Creating a new project..')
+                    pr_green("Creating a new project..")
                     clear_console(1)
-                    pr_green('Creating a new project...')
+                    pr_green("Creating a new project...")
                     clear_console(1)
                     projects_list = projects.load_projects_from_file(
                         projects_file_path)  # List of dictionaries
                     print("\033[36m {}\033[32m {}\033[36m {}\033[00m".format
                           ("Project created successfully.\nYou are now the "
-                           "leader of the project", f'{project_object.get_project_title()}', '.'))
+                           "leader of the project", f"{project_object.get_project_title()}", "."))
                     print("\033[36m {}\033[32m {}\033[00m".format
-                          ('Project ID: ', f'{project_object.get_project_id()}'))
+                          ("Project ID: ", f"{project_object.get_project_id()}"))
 
             elif ch_1 == "2":  # 2. My projects
                 if isinstance(projects.show_list_of_projects_and_choose(user), Project):
@@ -166,9 +164,9 @@ while run == true_bool:
 
             elif ch_1 == "5":  # 5. Exit
                 clear_console(1)
-                pr_green('Exiting program...')
+                pr_green("Exiting program...")
                 clear_console(2)
-                pr_green('Exit code: 0')
+                pr_green("Exit code: 0")
                 ch_1 = "0"
                 run = not true_bool
 
