1c71979146e0363321d0bdc7c6c21af643fe31d4
ParmisMiremadi <154410754+ParmisMiremadi@users.noreply.github.com>
2024-05-29 20:27:30 +0330

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -18,22 +18,17 @@ class Task:
         self.project_id = my_project.get_project_id()
         self.__task_id = uuid.uuid1()
         self.task_title = task_title
-        self.description = "" #. optional
+        self.description = "" #. optional/ adding later (manually)
         self.start_date = time.ctime(time.time())
         self.due_date = time.ctime(time.time() + 24 * 60 * 60)
         self.assignees = []
         self.priority = "LOW"
         self.status = "BACKLOG"
-        self.comments = {
-            "comment": "",
-            "user": "",
-            "date": ""
-        } #.??
 
     def get_unique_identifier(self):
         return self.__task_id
 
-    def to_dict_and_save_to_file(self, leader: User, my_project: Project):
+    def to_dict_and_save_to_file(self, my_project: Project):    # Returns my_project after updates
         new_task_dict = {
             "task_id": self.__task_id,
             "task_title": self.task_title,
@@ -42,9 +37,12 @@ class Task:
             "due_date": self.due_date,
             "assignees": self.assignees,
             "priority": self.priority,
-            "status": self.status,
-            "comments": self.comments
+            "status": self.status
         }
+
+        # Updating the project object
+        my_project.tasks.append(new_task_dict)
+
         # Saving task in 'projects.json'
         with open(projects_file_path, "r") as f:
             all_projects = json.load(f)
@@ -52,7 +50,7 @@ class Task:
         for it in range(len(all_projects)):
             if all_projects[it]["project_id"] == self.project_id:
                 print("tasks before: ", all_projects[it]["tasks"]) #.
-                all_projects[it]["tasks"].append(new_task_dict)
+                all_projects[it]["tasks"] = my_project.tasks
                 print("tasks after: ", all_projects[it]["tasks"]) #.
                 break
 
@@ -64,29 +62,54 @@ class Task:
             all_users = json.load(f)
 
         for it in range(len(all_users)):
-            if all_users[it]["username"] == my_project.leader_username:    # Finding the leader
+            # Updating the project for the leader of the project
+            if all_users[it]["username"] == my_project.leader_username:
                 leader_projects_as_leader = all_users[it]["projects_as_leader"]
                 for iterate in range(len(leader_projects_as_leader)):
                     if leader_projects_as_leader[iterate]["project_id"] == my_project.get_project_id():
+                        leader_projects_as_leader[iterate]["tasks"] = my_project.tasks
+                        break
 
+            # Updating the project for the members of the project
+            projects_as_member = all_users[it]["projects_as_member"]
+            if projects_as_member:
+                for i in range(len(projects_as_member)):
+                    if projects_as_member[i]["project_id"] == my_project.get_project_id():
+                        projects_as_member[i]["tasks"] = my_project.tasks
+                all_users[it]["projects_as_member"] = projects_as_member
 
+        with open(user_file_path, "w") as f:
+            json.dump(all_users, f, indent=4)
 
+        # Cases concerning the admin
+        with open(admin_file_path, "r") as f:
+            admin_list = json.load(f)
 
+        if admin_list:
+            # Admin is the leader of the project
+            if admin_list[0]["username"] == my_project.leader_username:
+                leader_projects_as_leader = admin_list[0]["projects_as_leader"]
+                for iterate in range(len(leader_projects_as_leader)):
+                    if leader_projects_as_leader[iterate]["project_id"] == my_project.get_project_id():
+                        leader_projects_as_leader[iterate]["tasks"] = my_project.tasks
+                        admin_list[0]["projects_as_leader"] = leader_projects_as_leader
+                        break
+            # Admin is a member of the project
+            projects_as_member = admin_list[0]["projects_as_member"]
+            if projects_as_member:
+                for iterate in range(len(projects_as_member)):
+                    if projects_as_member[iterate]["project_id"] == my_project.get_project_id():
+                        projects_as_member[iterate]["tasks"] = my_project.tasks
+                        admin_list[0]["projects_as_member"] = projects_as_member
+                        break
+
+            with open(admin_file_path, "w") as f:
+                json.dump(admin_list, f, indent=4)
+
+        return my_project
 
     def change_details(self): #. change obj data and save to files
         pass
 
-
-
-
-#############
-# input()
-#
-# task_1 = Task("mdc", "oijh")
-# print(time.gmtime())
-# print(time.time())
-# b = time.time()
-# print(b-a)
-# print(time.ctime(a))
-# print(f"start date: {task_1.start_date}")
-# print(f"due date: {task_1.due_date}")
+def create_a_task(my_project: Project):
+    
