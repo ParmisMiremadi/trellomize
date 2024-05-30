1dd3795c6ad6ccf62636b71bdf02e10fe487e7fa
mah05rad <154422590+mah05rad@users.noreply.github.com>
2024-05-30 15:38:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -40,11 +40,9 @@ true_bool = True
 while run == true_bool:
     choice = "0"  # 1. sign up  2. Log in
     while choice != "1" and choice != "2":
-        clear_console(2)
         print("1. Sign up")
         print("2. Log in")
         choice = input("Enter your choice: ")
-        os.system("cls")
 
         if choice == "1":
             user = sign_up()
@@ -55,29 +53,28 @@ while run == true_bool:
         else:
             pr_red("Error: Invalid choice! Please try again.")
             clear_console(2)
-    if user is None:
-        pr_red("Error: Invalid input! Please try again.")
-        clear_console(2)
 
-    elif isinstance(user, int) and user == 0:
-        print("Exiting program...")
-        clear_console(2)
-        pr_red("Exit code: 1")
-        run = not true_bool
+    if user is None:
+        if isinstance(user, float) and user == 1.72:
+            pr_red("Error: Your sign up was unsuccessful! Please try again.")
+            clear_console(2)
+
+        elif isinstance(user, float) and user == 3.14:
+            pr_red("Error: Your log in was unsuccessful! Please try again.")
+            clear_console(2)
 
     elif user.is_admin != true_bool:
         choice_1 = "0"  # 1. New project  2. My projects  3. Exit
         while choice_1 != "3":
             print("Choose one of the following options.")
-            pr_cyan("1. New project\n 2. My projects\n 3. Exit\n")
-            choice_1 = (input())
-            clear_console(1.5)
+            print("1. New project\n2. My projects\n3. Exit")
+            choice_1 = input("Enter your choice: ")
 
             if choice_1 == "1":    # 1. New project
+                os.system("cls")
                 project_object = projects.create_a_project(user)
                 if isinstance(project_object, Project):
                     user = project_object.to_dict_and_save_to_file(projects_file_path, user)
-                    time.sleep(1)
                     pr_green("Creating a new project")
                     clear_console(1)
                     pr_green("Creating a new project.")
@@ -89,16 +86,16 @@ while run == true_bool:
                     # List of dictionaries:
                     projects_list = projects.load_projects_from_file(projects_file_path)
                     print("\033[36m {}\033[32m {}\033[36m {}\033[00m".format
-                          ("Project created successfully.\nYou are now the "
+                          ("Project created successfully.\n You are now the "
                            "leader of the project", f"{project_object.get_project_title()}", "."))
                     print("\033[36m {}\033[32m {}\033[00m".format
-                          ("Project ID: ", f"{project_object.get_project_id()}"))
-
+                          ("Project ID:", f"{project_object.get_project_id()}"))
+                    clear_console(5)
                 else:
                     pass
 
             elif choice_1 == "2":    # 2. My projects
-                clear_console(2)
+                os.system("cls")
                 if isinstance(projects.show_list_of_projects_and_choose(user), Project):
                     print(f"2 __ The user: {user.username}")  #.
                     print(f"2 __ The leader: {my_project.leader_username}")  #.
@@ -112,27 +109,28 @@ while run == true_bool:
                     choice_1 = "0"
 
             elif choice_1 == "3":    # 3. Exit
-                clear_console(1)
+                os.system("cls")
                 pr_green("Exiting program...")
                 clear_console(2)
                 pr_green("Exit code: 0")
                 run = not true_bool
 
             else:
-                print("Invalid choice.\nPlease try again.")
+                pr_red("Error: Invalid choice! Please try again.")
+                clear_console(2)
 
     elif user.is_admin == true_bool:
         ch_1 = "-1"  # 1. New project  2. My projects  3. Activate users 4. Deactivate users 5. Exit
         while ch_1 != "0":
             print("Choose one of the following options.")
-            pr_cyan("1. New project\n 2. My projects\n 3. Activate users\n 4. Deactivate users\n 5. Exit\n")
-            ch_1 = input()
+            print("1. New project\n2. My projects\n3. Activate users\n4. Deactivate users\n5. Exit")
+            ch_1 = input("Enter your choice: ")
 
             if ch_1 == "1":  # 1. New project
+                os.system("cls")
                 project_object = projects.create_a_project(user)
                 if isinstance(project_object, Project):
                     user = project_object.to_dict_and_save_to_file(projects_file_path, user)
-                    time.sleep(1)
                     pr_green("Creating a new project")
                     clear_console(1)
                     pr_green("Creating a new project.")
@@ -144,12 +142,17 @@ while run == true_bool:
                     projects_list = projects.load_projects_from_file(
                         projects_file_path)  # List of dictionaries
                     print("\033[36m {}\033[32m {}\033[36m {}\033[00m".format
-                          ("Project created successfully.\nYou are now the "
+                          ("Project created successfully.\n You are now the "
                            "leader of the project", f"{project_object.get_project_title()}", "."))
                     print("\033[36m {}\033[32m {}\033[00m".format
-                          ("Project ID: ", f"{project_object.get_project_id()}"))
+                          ("Project ID:", f"{project_object.get_project_id()}"))
+                    clear_console(5)
+                
+                else:
+                    pass
 
             elif ch_1 == "2":  # 2. My projects
+                os.system("cls")
                 if isinstance(projects.show_list_of_projects_and_choose(user), Project):
                     my_project = projects.show_list_of_projects_and_choose(user)
                     user, project_object = projects.options_for_my_project(user, my_project)
@@ -157,13 +160,15 @@ while run == true_bool:
                     ch_1 = 0
 
             elif ch_1 == "3":  # 3. Activate users
+                os.system("cls")
                 projects.activate_users(users_list)
 
             elif ch_1 == "4":  # 4. Deactivate users
+                os.system("cls")
                 projects.deactivate_users(users_list)
 
             elif ch_1 == "5":  # 5. Exit
-                clear_console(1)
+                os.system("cls")
                 pr_green("Exiting program...")
                 clear_console(2)
                 pr_green("Exit code: 0")
@@ -171,5 +176,12 @@ while run == true_bool:
                 run = not true_bool
 
             else:
-                pr_red("Error: Invalid value!")
+                pr_red("Error: Invalid choice! Please try again.")
+                clear_console(2)
                 ch_1 = "-1"
+    
+    else:
+        print("Exiting program...")
+        clear_console(2)
+        pr_red("Exit code: 1")
+        run = not true_bool
